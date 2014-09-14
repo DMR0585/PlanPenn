87954da8c58ff213c1ab034bc1c675ff9cb31130
Daniel Blank <danblank@seas.upenn.edu>
2014-09-13 22:34:33 -0400

    something i changed

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,2 +1,8 @@
 class UserController < ApplicationController
+	def index
+	end
+
+	def login
+	redirect_to controller: "main", action: "index"	
+	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,19 @@
 Rails.application.routes.draw do
+  post 'register/new', to: 'register#create', as: 'register'
+
+  post 'user/index', to: 'user#login', as: 'user'
+
   get 'main/index'
+
+  get 'user/index'
+
+  get 'register/new'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'user#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
